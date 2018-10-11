9c6132a391c2f497cf57602751b424434cda5571
ggolish <ggolish@sycamores.indstate.edu>
2018-10-11 16:06:23 -0400

    Testing send function.

--- common/src/error.c
+++ common/src/error.c
@@ -8,3 +8,9 @@ void die(int exit_code)
     fprintf(stderr, "Error: %s\n", err_msg);
     exit(exit_code);
 }
+
+void usage(char *argv[])
+{
+    fprintf(stderr, "Usage: %s <network-device>\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
