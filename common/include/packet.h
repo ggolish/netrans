0b3b5d853bf9450bbe4037f3cc04bdb38ed5c560
ggolish <ggolish@sycamores.indstate.edu>
2018-10-10 23:28:10 -0400

    Added definition of ethernet header and defined netrans ethertype.

--- common/include/packet.h
+++ common/include/packet.h
@@ -1,6 +1,15 @@
-#ifndef NETRANS_PACKET_H
-#define NETRANS_PACKET_H
+#ifndef PACKETS_H_
+#define PACKETS_H_
 
+#include <stdint.h>
 
+#define ETHER_TYPE_NETRANS 0x0929
+
+// Ethernet packet header
+typedef struct __attribute__((packed)) {
+    uint8_t  eth_mac_dest[6]; // The destination MAC address
+    uint8_t  eth_mac_src[6];  // The source MAC address
+    uint16_t eth_type;        // The type of the payload
+} PACKET_ETH_HDR;
 
 #endif
