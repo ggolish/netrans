c65466600e0e3858f8ad0321e23f071bc89717f1
ggolish <ggolish@sycamores.indstate.edu>
2018-10-16 23:59:58 -0400

    Defined netrans packet header

--- common/include/packet.h
+++ common/include/packet.h
@@ -12,4 +12,16 @@ typedef struct __attribute__((packed)) {
     uint16_t eth_type;        // The type of the payload
 } PACKET_ETH_HDR;
 
+#define NETRANS_TYPE_SEND     0x01
+#define NETRANS_TYPE_RECEIVE  0x02
+#define NETRANS_TYPE_ACK      0x03
+
+typedef struct __attribute__((packed)) {
+    uint8_t netrans_src;
+    uint8_t netrans_dest;
+    uint8_t netrans_type;
+} PACKET_NETRANS_HDR;
+
+#define NETRANS_PAYLOAD_CHUNK 512
+
 #endif
